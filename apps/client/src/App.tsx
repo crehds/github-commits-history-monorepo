6ffb0a8f7ed5bf0c156ef92562cf5dee6a46e62b
Carlos Rodriguez <carlos15erh@gmail.com>
2023-09-27 02:12:04 -0500

    add logic to get commits from api
    use useEffect and useState

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Commit } from './dto/commits.dto';
 
 function App() {
+  const [commits, setCommits] = useState<Commit[]>([]);
+  useEffect(() => {
+    fetch('/api/commits/crehds/github-commits-history-monorepo')
+      .then((res) => res.json())
+      .then((data) => setCommits(data));
+  });
   return (
-    <button onClick={async () => {
-      const response = await fetch('/api');
-      const data = await response.text();
-      console.log(data);
-    }}>
-      <span>Click me!</span>
-    </button>
+    <main>
+      <h1>Commits</h1>
+      <ul>
+        {commits.map((commit) => (
+          <li key={commit.sha}>{commit.commit.message}</li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
