7136d6d4a27e114bc815def1c1c083f063befc04
Carlos Rodriguez <carlos15erh@gmail.com>
2023-09-27 05:22:52 -0500

    styles(client): convert CommitItems into expandable cards

--- apps/client/src/components/CommitItem.tsx
+++ apps/client/src/components/CommitItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Commit } from "../dto/commits.dto";
 
 interface Props {
@@ -5,9 +6,29 @@ interface Props {
 }
 
 function CommitItem({commit}: Props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div>
-      <p>{commit.commit.message}</p>
+    <div className="border rounded-lg shadow-md p-4 mb-4">
+      <div className="flex justify-between items-center">
+        <h3 className="text-lg font-medium">{commit.commit.message}</h3>
+        <button
+          className="text-blue-500 hover:text-gray-500 focus:outline-none focus:underline focus:text-gray-300 transition duration-250 ease-in-out"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          {isExpanded ? "Hide" : "Show"}
+        </button>
+      </div>
+      {
+        isExpanded && (
+          <div className="mt-4">
+            <p className="text-blue-600">{commit.sha}</p>
+            <p className="text-blue-600">{commit.commit.author.name}</p>
+            <p className="text-blue-600">{commit.commit.author.email}</p>
+            <p className="text-blue-600">{commit.commit.author.date}</p>
+          </div>
+        )
+      }
     </div>
   );
 }
