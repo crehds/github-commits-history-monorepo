a901d7840ff7d03beb2a2029e3e189dcf81d0ba0
Carlos Rodriguez <carlos15erh@gmail.com>
2023-09-27 08:07:33 -0500

    test(api): add missing providers to fix commits service test

--- apps/api/src/commits/commits.service.spec.ts
+++ apps/api/src/commits/commits.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommitsService } from './commits.service';
+import { Octokit } from '@octokit/rest';
+import { ConfigService } from '@nestjs/config';
 
 describe('CommitsService', () => {
   let service: CommitsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommitsService],
+      providers: [CommitsService, Octokit, ConfigService],
     }).compile();
 
     service = module.get<CommitsService>(CommitsService);
