004c80e67bcaa9bd069c5cd05e9731b22bb39993
Carlos Rodriguez <carlos15erh@gmail.com>
2023-09-27 05:01:36 -0500

    (client) update Commit dto with new properties to handle the author

--- apps/client/src/dto/commits.dto.ts
+++ apps/client/src/dto/commits.dto.ts
@@ -2,5 +2,14 @@ export interface Commit {
   sha: string;
   commit: {
     message: string
+    author: {
+      name: string;
+      email: string;
+      date: string;
+    }
+  },
+  author: {
+    login: string;
+    avatar_url: string;
   }
 }
