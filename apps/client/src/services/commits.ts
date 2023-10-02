7aedaf9c7ae9bfb1dd7584d50ba8da486f9416e9
Carlos Rodriguez <carlos15erh@gmail.com>
2023-10-02 01:32:30 -0500

    feat(app): add since and until query params to filter commits

--- apps/client/src/services/commits.ts
+++ apps/client/src/services/commits.ts
@@ -7,6 +7,8 @@ export const getCommitsRequest = (repo: string, filters: Filters) => {
   const searchParams = new URLSearchParams();
 
   searchParams.append('perPage', filters.perPage.toString());
+  searchParams.append('since', filters.since.toString());
+  searchParams.append('until', filters.until.toString());
 
   return fetch(`${API}/${repo}?${searchParams.toString()}`);
 };
