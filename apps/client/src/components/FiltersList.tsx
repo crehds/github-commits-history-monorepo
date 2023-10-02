35ca722c84c772ff175d4cb75e8c1f7cb6f0dbfc
Carlos Rodriguez <carlos15erh@gmail.com>
2023-10-02 03:00:48 -0500

    style(client): improve layout of filters list component

--- apps/client/src/components/FiltersList.tsx
+++ apps/client/src/components/FiltersList.tsx
@@ -16,20 +16,22 @@ function FiltersList({ onFilter: onFilter }: Props) {
   };
 
   return (
-    <div>
-      <label>
-        Per page:
-        <input type="number" value={perPage} onChange={(e) => setPerPage(e.target.value)} />
-      </label>
-      <label>
-        Since:
-        <input type="text" value={since} onChange={(e) => setSince(e.target.value)} />
-      </label>
-      <label>
-        Until:
-        <input type="text" value={until} onChange={(e) => setUntil(e.target.value)} />
-      </label>
-      <button onClick={handleFilter}>Filter</button>
+    <div className='flex justify-center'>
+      <div className="bg-gray-800 rounded-lg shadow-lg overflow-hidden flex flex-col gap-3 px-4 pt-6 pb-1">
+        <label className='grid gap-3 grid-cols-[1fr_2fr]'>
+          <p className='text-center p-1 font-bold' >Per page</p>
+          <input type="number" value={perPage} onChange={(e) => setPerPage(e.target.value)} className='border border-red-50 border-solid rounded-md px-3 py-1'/>
+        </label>
+        <label className='grid gap-3 grid-cols-[1fr_2fr]'>
+          <p className='text-center p-1 font-bold'>Since</p>
+          <input type="date" value={since} onChange={(e) => setSince(e.target.value)} className='border border-red-50 border-solid  rounded-md px-3 py-1'/>
+        </label>
+        <label className='grid gap-3 grid-cols-[1fr_2fr]'>
+          <p className='text-center p-1 font-bold'>Until</p>
+          <input type="date" value={until} onChange={(e) => setUntil(e.target.value)} className='border border-red-50 border-solid  rounded-md px-3 py-1'/>
+        </label>
+        <button onClick={handleFilter}>Filter</button>
+      </div>
     </div>
   );
 }
