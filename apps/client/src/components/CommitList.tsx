48f9937fa4494957413e88d1dc63ff5349169cfa
Carlos Rodriguez <carlos15erh@gmail.com>
2023-10-02 05:21:55 -0500

    feat(client): add spinner component to improve UX

--- apps/client/src/components/CommitList.tsx
+++ apps/client/src/components/CommitList.tsx
@@ -1,5 +1,6 @@
 import { Filters } from "../dto/filters.dto";
 import { useGetCommits } from "../hooks/useGetCommits";
+import { Spinner } from "../spinners/spinner";
 import CommitItem from "./CommitItem";
 import FiltersList from "./FiltersList";
 
@@ -10,10 +11,10 @@ function CommitList() {
   };
 
   return (
-    <div className="flex flex-col justify-center gap-3">
+    <div className="flex flex-col justify-center gap-3 items-center">
       <h1 className="tracking-wide text-5xl">Commits</h1>
       <FiltersList onFilter={onFilter} />
-      {isLoading && <p>Loading...</p>}
+      {isLoading && <Spinner />}
       {error ? <p>Error</p> : null}
       <ul>
         {commits.map((commit) => (
