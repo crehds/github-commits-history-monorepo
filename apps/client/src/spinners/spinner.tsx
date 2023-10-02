1d5166e85998e9b5516a38a9f5e2a7731e32e9eb
Carlos Rodriguez <carlos15erh@gmail.com>
2023-10-02 05:21:16 -0500

    (client): create spinner component to represent loading state

---
+++ apps/client/src/spinners/spinner.tsx
@@ -0,0 +1,5 @@
+import './spinner.css';
+
+export function Spinner() {
+  return <div className="border-4 border-solid border-opacity-30 border-gray-300 border-l-blue-700 w-9 h-9 rounded-full border-left-primary-3 animate-spin duration-1000 linear infinite m-4"/>;
+}
